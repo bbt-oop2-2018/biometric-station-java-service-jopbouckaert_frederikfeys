60be06da836950ae15513f54fa9c84a16fe10739
Frederik Feys <frederik.feys99@gmail.com>
2018-05-18 09:19:27 +0200

    Final version

--- BioMetricStationSerialReceiver/src/biometricstationserialreceiver/BioMetricStationStringParser.java
+++ BioMetricStationSerialReceiver/src/biometricstationserialreceiver/BioMetricStationStringParser.java
@@ -5,27 +5,27 @@ package biometricstationserialreceiver;
  * @author jopbo_000
  */
 public class BioMetricStationStringParser {
-
+    
     public SensorData parse(String dataString) {
-
+        final double CHECKSUM_TOLERANCE = 0.01;
         if (!isValidString(dataString)) {
             return null;
-        } else {
+        }
 
-            int temperatureStart = dataString.indexOf("ABCDE");
-            int heartBeatStart = dataString.indexOf("|");
-            int xAcceleroStart = dataString.indexOf("XXX");
-            int yAcceleroStart = dataString.indexOf("YYY");
-            int zAcceleroStart = dataString.indexOf("ZZZ");
-            int checksumStart = dataString.indexOf("CCC");
-            int checksumEnd = dataString.indexOf("EDCBA");
-            String temperatureString = dataString.substring(temperatureStart + 5, heartBeatStart);
-            String heartBeatString = dataString.substring(heartBeatStart + 1, xAcceleroStart);
-            String xAcceleroString = dataString.substring(xAcceleroStart + 3, yAcceleroStart);
-            String yAcceleroString = dataString.substring(yAcceleroStart + 3, zAcceleroStart);
-            String zAcceleroString = dataString.substring(zAcceleroStart + 3, checksumStart);
-            String checksumString = dataString.substring(checksumStart + 3, checksumEnd);
-
+        int temperatureStart = dataString.indexOf("ABCDE");
+        int heartBeatStart = dataString.indexOf("|");
+        int xAcceleroStart = dataString.indexOf("XXX");
+        int yAcceleroStart = dataString.indexOf("YYY");
+        int zAcceleroStart = dataString.indexOf("ZZZ");
+        int checksumStart = dataString.indexOf("CCC");
+        int checksumEnd = dataString.indexOf("EDCBA");
+        String temperatureString = dataString.substring(temperatureStart + 5, heartBeatStart);
+        String heartBeatString = dataString.substring(heartBeatStart + 1, xAcceleroStart);
+        String xAcceleroString = dataString.substring(xAcceleroStart + 3, yAcceleroStart);
+        String yAcceleroString = dataString.substring(yAcceleroStart + 3, zAcceleroStart);
+        String zAcceleroString = dataString.substring(zAcceleroStart + 3, checksumStart);
+        String checksumString = dataString.substring(checksumStart + 3, checksumEnd);
+        try {
             double temperature = Double.parseDouble(temperatureString);
             double heartBeat = Double.parseDouble(heartBeatString);
             double xAccelero = Double.parseDouble(xAcceleroString);
@@ -33,14 +33,15 @@ public class BioMetricStationStringParser {
             double zAccelero = Double.parseDouble(zAcceleroString);
             double checksum = Double.parseDouble(checksumString);
 
-            if (temperature + heartBeat + xAccelero + yAccelero + zAccelero == checksum) {
+            if (Math.abs(temperature + heartBeat + xAccelero + yAccelero + zAccelero - checksum) <= CHECKSUM_TOLERANCE) {
                 return new SensorData(temperature, heartBeat, xAccelero, yAccelero, zAccelero);
-
-            } else {
-                return null;
             }
+        } catch (Exception e) {
+           System.out.println("Received invalid data has been thrown");
+            return null;
         }
-
+        
+        return null;
     }
 
     private boolean isValidString(String dataString) {
